f766cd3dc5a5cdb789cdabad5efe511c6fc5fbc3
Anuj Arora <anujarora920804@gmail.com>
2021-04-29 14:57:12 +0900

    Add custom trainer class

--- cifar/trainer/combined_trainer.py
+++ cifar/trainer/combined_trainer.py
@@ -1,3 +1,21 @@
 """Module implementing custom model.fit for combined model training"""
 
 import tensorflow.keras.models as KM
+
+
+class Trainer(KM.Model):
+    def __init__(self):
+        super(Trainer, self).__init__()
+
+    def compile(self):
+        super(Trainer, self).compile()
+
+    def call(self, *args, **kwargs):
+        super(Trainer, self).call(*args, **kwargs)
+
+    def train_step(self, *args, **kwargs):
+        super(Trainer, self).train_step(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    trainer = Trainer()
