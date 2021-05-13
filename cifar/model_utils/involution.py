9ae15b50dd302b469f4f5e7bd70f13166edf5d2c
Anuj Arora <anujarora920804@gmail.com>
2021-05-13 16:35:32 +0900

    issue with dynamic shape for KM.Model

--- cifar/model_utils/involution.py
+++ cifar/model_utils/involution.py
@@ -5,12 +5,14 @@ import tensorflow.keras.layers as KL
 import tensorflow.keras.models as KM
 
 
-def involution(
-    filters: int,
-    kernel_size: int,
+# pylint: disable=R0913
+def involution_model(
+    filters: int = 32,
+    kernel_size: int = 3,
     strides: int = 1,
     reduction: int = 4,
     group_channels: int = 16,
+    name: str = "",
 ) -> KM.Model:
     """involution module as proposed by Duo Li et al. in
     Involution: Inverting the Inherence of Convolution for Visual Recognition
@@ -71,13 +73,95 @@ def involution(
 
     # reshape back to 4D tensor (Batch x Height x width x channels)
     out = tf.reshape(out, shape=(b_size, h_tensor, w_tensor, filters))
+    print(out)
 
-    return KM.Model(inputs=input_tensor, outputs=out)
+    return KM.Model(inputs=input_tensor, outputs=out, name=f"invol_{name}")
+
+
+def involution(
+    filters: int = 32,
+    kernel_size: int = 3,
+    strides: int = 1,
+    reduction: int = 4,
+    group_channels: int = 16,
+    name: str = "",
+):
+    """involution module as proposed by Duo Li et al. in
+    Involution: Inverting the Inherence of Convolution for Visual Recognition
+
+    Args:
+        filters (int): number of filters for the incoming and outgoing tensor
+        kernel_size (int): kernel size for the involution
+        strides (int, optional): strides for involution. Defaults to 1.
+        reduction (int, optional): channel reduction for dynamic weights calculation. Defaults to 4.
+        group_channels (int, optional): number of channels in a group. Defaults to 16.
+        name (str, optional): string for layer name. Defaults to ""
+
+    Returns:
+        involution function
+    """
+    # pylint: disable=unnecessary-lambda
+    def layer(input_tensor: tf.Tensor) -> tf.Tensor:
+
+        # number of groups
+        groups = filters // group_channels
+
+        if strides > 1:
+            avgpool = KL.AveragePooling2D(pool_size=(strides, strides))
+        else:
+            avgpool = KL.Lambda(lambda x: tf.identity(x))
+
+        # weights for involution kernel
+        weight = KL.Conv2D(filters // reduction, kernel_size=1, use_bias=False)(
+            avgpool(input_tensor)
+        )
+        weight = KL.Conv2D(
+            kernel_size ** 2 * groups,
+            kernel_size=1,
+            strides=1,
+        )(KL.Activation("relu")(KL.BatchNormalization()(weight)))
+
+        # Batch x Height x width x channels
+        b_size, h_tensor, w_tensor, _ = tf.shape(weight)
+        weight = tf.reshape(
+            weight, shape=(b_size, kernel_size ** 2, h_tensor, w_tensor, groups, 1)
+        )
+
+        # tensor patches for each involution kernel
+        out = tf.image.extract_patches(
+            input_tensor,
+            sizes=[1, kernel_size, kernel_size, 1],
+            strides=[1, 1, 1, 1],
+            rates=[1, 1, 1, 1],
+            padding="SAME",
+        )
+
+        # involution kernels are broadcasted over the "group_channels"
+        out = tf.reshape(
+            out,
+            shape=(
+                b_size,
+                kernel_size ** 2,
+                h_tensor,
+                w_tensor,
+                groups,
+                group_channels,
+            ),
+        )
+        # sum over kernel dimension
+        out = tf.reduce_sum(weight * out, axis=1)
+
+        # reshape back to 4D tensor (Batch x Height x width x channels)
+        out = tf.reshape(
+            out, shape=(b_size, h_tensor, w_tensor, filters), name=f"invol_{name}"
+        )
+        return out
+
+    return layer
 
 
 if __name__ == "__main__":
     a = tf.random.uniform(shape=(2, 224, 224, 32), dtype=tf.float32)
 
     model = involution(32, 7)
-    model.summary()
     print(model(a))
